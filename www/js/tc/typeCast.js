dd4253b9b563c655d8d682aea02aa8a617a24057
Dinesh <dinesh.condur@gmail.com>
2017-11-05 18:01:16 -0500

    Delivery build

---
+++ www/js/tc/typeCast.js
@@ -0,0 +1,533 @@
+/* typeCast keyboard API for Mapping physical keyboard to different layouts
+ *
+ */
+
+"use strict"
+
+//All globals
+let currentClient, uLayout, tLayout, outp = "";
+let typeCastKeyboard = false, caps = false, shift = false;
+let row , pos = ""; 
+
+
+//Client Config class to store client specific details
+// storing some of the interesting properties from navigator object to a clientConfig object
+function getClientDetails() {
+  class clientConfig {
+    constructor() {
+      //get defaults from navigator object
+      this.language = navigator.language;
+      this.languagesAvail = navigator.languages;
+      this.userAgent = navigator.userAgent;
+    }
+
+  }
+
+  currentClient = new clientConfig();
+  // console.log(currentClient);
+}
+
+//Step0: get client details can be useful during client testing
+getClientDetails();
+
+
+//Step1: Identify user template assume US-en for iterations 
+//load user layout from the language set in the browser   
+loadUserLayout(currentClient.language);
+
+
+
+
+//Step2: Runn Init() 
+//register event handlers
+//Global switch for the keyboard
+
+function init() {
+  window.addEventListener("keydown", keydownHandler, false);
+  window.addEventListener("keypress", keypressHandler, false);
+  window.addEventListener("keyup", keyupHandler, false);
+
+  // window.addEventListener("input", changeHandler, false);
+  // window.addEventListener("paste", changeHandler, false);
+
+}
+
+init();
+
+//Step 2.1: Listen for the switch and listen when only required
+//disable event bubbling and default behaviour
+// function switchMode() {
+//   if (typeCastKeyboard == false) {
+//     typeCastKeyboard = true;
+//     // debugger;
+//   }
+//   else if (typeCastKeyboard == true) {
+//     typeCastKeyboard = false;
+//   }
+// }
+
+
+function toggle(key) {
+  // debugger;
+  if (key === false) {
+    return key = true;
+    // debugger;
+  }
+  else if (key === true) {
+    return key = false;
+  }
+}
+
+
+
+function keyupHandler(event) {
+
+  //if esc is pressed toggle keyboard
+  //Switch keyboard On/Off 
+
+  if (event.keyCode === 27) { //on the keyUp Event of Esc key  
+    typeCastKeyboard = toggle(typeCastKeyboard);
+    // debugger;
+  }
+
+  //when keyboard ON get caps key state
+  if (typeCastKeyboard && event.keyCode === 20) {
+    caps = event.getModifierState("CapsLock");
+    // console.log(caps);
+  }
+
+
+  //show virtual keyboard on screen
+  if (typeCastKeyboard && event.keyCode === 38) {
+    console.log("Show Virtual keyboard");
+    // console.log(caps);
+  }
+
+
+
+}
+
+
+function keydownHandler(event) {
+
+}
+
+
+
+function keypressHandler(event) {
+
+  //only capture key codes and not modifier keys
+  //only capture events when keyboard is on
+  if (typeCastKeyboard) {
+    event.stopPropagation();    //Avoid event bubbling
+    event.preventDefault();     //Prevent user inputs to text box ..Check for mobile inputs
+    // console.log(event);
+    //proceed further to change text from here;
+
+    //Step3: Load the target template from a URL -- add AJAX call to the JSON file -- use local json or JS files -- Cache as web Worker for offline access
+    // getTargetLayout(lang);
+    //did this from appInit to make calls easier
+
+    //Step4: Run mapLayout function(with user and target layouts) Map the user layout to target layout
+    let inp = {
+      which: event.which,
+      alt: event.altKey,
+      charCode: event.charCode,
+      code: event.code,
+      ctrlKey: event.ctrlKey,
+      key: event.key,
+      keyCode: event.keyCode,
+      location: event.location,
+      shiftKey: event.shiftKey,
+      srcElement: event.srcElement,
+      target: event.target,
+      timeStamp: event.timeStamp,
+      capsKey: event.getModifierState("CapsLock")
+    }
+
+
+    //intialise with exiting text on the input / src field
+    //assign the new value from our layout mapping
+    outp = inp.srcElement.value + mapLayout(inp, uLayout, tLayout);
+    inp.target.value = outp;
+
+
+    //Step5: Print on the <event.target> element (no need to specify input elements) on the text field
+    //Step6: Show on screen keyboard mapped to the typeCast keyboard bool -- map the mouse events to the virtual keyboard.
+    //Step7: Check for mobile device access
+    //Step8: Track for feature requests
+
+  }
+  else if (false) { //<-- condition to inhibit
+    //Inhibit function
+    //remove Handler when not in use make sure about removing keypress handler here
+
+    // window.removeEventListener("keydown", keydownHandler, false);
+
+  }
+
+
+}
+
+
+
+
+
+function loadUserLayout(browserLang) {
+  // Use a AJAX request here to fetch a JSON file from server // load only the ones required
+  //create a instance of class for each of the available layouts
+  //set userLayout and targetlayouts
+  // console.log(browserLang);
+
+  let userLayout = {
+    'en': {
+      'name': 'en',
+      'lang': ['all'],
+      'normal': [
+        '` 1 2 3 4 5 6 7 8 9 0 - = \u0008',
+        '\u0009 q w e r t y u i o p [ ] \\',
+        'a s d f g h j k l ; \' {enter}',
+        '{shift} z x c v b n m , . / {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'shift': [
+        '~ ! @ # $ % ^ & * ( ) _ + \u0008',
+        '\u0009 Q W E R T Y U I O P { } |',
+        'A S D F G H J K L : " {enter}',
+        '{shift} Z X C V B N M < > ? {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt': [
+        '~ \u00a1 \u00b2 \u00b3 \u00a4 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00d7 \u0008',
+        '\u0009 \u00e4 \u00e5 \u00e9 \u00ae \u00fe \u00fc \u00fa \u00ed \u00f3 \u00f6 \u00ab \u00bb \u00ac',
+        '\u00e1 \u00df \u00f0 f g h j k \u00f8 \u00b6 \u00b4 {enter}',
+        '{shift} \u00e6 x \u00a9 v b \u00f1 \u00b5 \u00e7 > \u00bf {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt-shift': [
+        '~ \u00b9 \u00b2 \u00b3 \u00a3 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00f7 \u0008',
+        '\u0009 \u00c4 \u00c5 \u00c9 \u00ae \u00de \u00dc \u00da \u00cd \u00d3 \u00d6 \u00ab \u00bb \u00a6',
+        '\u00c4 \u00a7 \u00d0 F G H J K \u00d8 \u00b0 \u00a8 {enter}',
+        '{shift} \u00c6 X \u00a2 V B \u00d1 \u00b5 \u00c7 . \u00bf {shift}',
+      ]
+    },
+    'en-CA': {
+      'name': 'en-CA',
+      'lang': ['all'],
+      'normal': [
+        '` 1 2 3 4 5 6 7 8 9 0 - = \u0008',
+        '\u0009 q w e r t y u i o p [ ] \\',
+        'a s d f g h j k l ; \' {enter}',
+        '{shift} z x c v b n m , . / {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'shift': [
+        '~ ! @ # $ % ^ & * ( ) _ + \u0008',
+        '\u0009 Q W E R T Y U I O P { } |',
+        'A S D F G H J K L : " {enter}',
+        '{shift} Z X C V B N M < > ? {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt': [
+        '~ \u00a1 \u00b2 \u00b3 \u00a4 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00d7 \u0008',
+        '\u0009 \u00e4 \u00e5 \u00e9 \u00ae \u00fe \u00fc \u00fa \u00ed \u00f3 \u00f6 \u00ab \u00bb \u00ac',
+        '\u00e1 \u00df \u00f0 f g h j k \u00f8 \u00b6 \u00b4 {enter}',
+        '{shift} \u00e6 x \u00a9 v b \u00f1 \u00b5 \u00e7 > \u00bf {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt-shift': [
+        '~ \u00b9 \u00b2 \u00b3 \u00a3 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00f7 \u0008',
+        '\u0009 \u00c4 \u00c5 \u00c9 \u00ae \u00de \u00dc \u00da \u00cd \u00d3 \u00d6 \u00ab \u00bb \u00a6',
+        '\u00c4 \u00a7 \u00d0 F G H J K \u00d8 \u00b0 \u00a8 {enter}',
+        '{shift} \u00c6 X \u00a2 V B \u00d1 \u00b5 \u00c7 . \u00bf {shift}',
+      ]
+    },
+    'en-US': {
+      'name': 'en-US',
+      'lang': ['all'],
+      'normal': [
+        '` 1 2 3 4 5 6 7 8 9 0 - = \u0008',
+        '\u0009 q w e r t y u i o p [ ] \\',
+        'a s d f g h j k l ; \' \u000A',
+        '{shift} z x c v b n m , . / {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'shift': [
+        '~ ! @ # $ % ^ & * ( ) _ + \u0008',
+        '\u0009 Q W E R T Y U I O P { } |',
+        'A S D F G H J K L : " \u000A',
+        '{shift} Z X C V B N M < > ? {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt': [
+        '~ \u00a1 \u00b2 \u00b3 \u00a4 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00d7 \u0008',
+        '\u0009 \u00e4 \u00e5 \u00e9 \u00ae \u00fe \u00fc \u00fa \u00ed \u00f3 \u00f6 \u00ab \u00bb \u00ac',
+        '\u00e1 \u00df \u00f0 f g h j k \u00f8 \u00b6 \u00b4 \u000A',
+        '{shift} \u00e6 x \u00a9 v b \u00f1 \u00b5 \u00e7 > \u00bf {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt-shift': [
+        '~ \u00b9 \u00b2 \u00b3 \u00a3 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00f7 \u0008',
+        '\u0009 \u00c4 \u00c5 \u00c9 \u00ae \u00de \u00dc \u00da \u00cd \u00d3 \u00d6 \u00ab \u00bb \u00a6',
+        '\u00c4 \u00a7 \u00d0 F G H J K \u00d8 \u00b0 \u00a8 \u000A',
+        '{shift} \u00c6 X \u00a2 V B \u00d1 \u00b5 \u00c7 . \u00bf {shift}',
+      ]
+    },
+    'en-GB': {
+      'name': 'en-GB',
+      'lang': ['all'],
+      'normal': [
+        '` 1 2 3 4 5 6 7 8 9 0 - = \u0008',
+        '\u0009 q w e r t y u i o p [ ] \\',
+        'a s d f g h j k l ; \' {enter}',
+        '{shift} z x c v b n m , . / {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'shift': [
+        '~ ! @ # $ % ^ & * ( ) _ + \u0008',
+        '\u0009 Q W E R T Y U I O P { } |',
+        'A S D F G H J K L : " {enter}',
+        '{shift} Z X C V B N M < > ? {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt': [
+        '~ \u00a1 \u00b2 \u00b3 \u00a4 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00d7 \u0008',
+        '\u0009 \u00e4 \u00e5 \u00e9 \u00ae \u00fe \u00fc \u00fa \u00ed \u00f3 \u00f6 \u00ab \u00bb \u00ac',
+        '\u00e1 \u00df \u00f0 f g h j k \u00f8 \u00b6 \u00b4 {enter}',
+        '{shift} \u00e6 x \u00a9 v b \u00f1 \u00b5 \u00e7 > \u00bf {shift}',
+        '{accept} {alt} {space} {alt} {cancel}'
+      ],
+      'alt-shift': [
+        '~ \u00b9 \u00b2 \u00b3 \u00a3 \u20ac \u00bc \u00bd \u00be \u2018 \u2019 \u00a5 \u00f7 \u0008',
+        '\u0009 \u00c4 \u00c5 \u00c9 \u00ae \u00de \u00dc \u00da \u00cd \u00d3 \u00d6 \u00ab \u00bb \u00a6',
+        '\u00c4 \u00a7 \u00d0 F G H J K \u00d8 \u00b0 \u00a8 {enter}',
+        '{shift} \u00c6 X \u00a2 V B \u00d1 \u00b5 \u00c7 . \u00bf {shift}',
+      ]
+    },
+    'ru': {
+      "name": "ms-Russian",
+      "lang": ["ru"],
+      "normal": [
+        "\u0451 1 2 3 4 5 6 7 8 9 0 - = \u0008",
+        "\u0009 \u0439 \u0446 \u0443 \u043a \u0435 \u043d \u0433 \u0448 \u0449 \u0437 \u0445 \u044a \\",
+        "\u0444 \u044b \u0432 \u0430 \u043f \u0440 \u043e \u043b \u0434 \u0436 \u044d \u000D",
+        "{shift} \\ \u044f \u0447 \u0441 \u043c \u0438 \u0442 \u044c \u0431 \u044e / {shift}",
+        "{accept} {space} {cancel}"
+      ],
+      "shift": [
+        "\u0401 ! \" \u2116 ; % : ? * ( ) _ + \u0008",
+        "\u0009 \u0419 \u0426 \u0423 \u041a \u0415 \u041d \u0413 \u0428 \u0429 \u0417 \u0425 \u042a /",
+        "\u0424 \u042b \u0412 \u0410 \u041f \u0420 \u041e \u041b \u0414 \u0416 \u042d \u000D",
+        "{shift} / \u042f \u0427 \u0421 \u041c \u0418 \u0422 \u042c \u0411 \u042e / {shift}",
+        "{accept} {space} {cancel}"
+      ]
+    },
+    "fr": {
+      'name': 'french-azerty-1',
+      'lang': ['fr'],
+      'normal': [
+        "\u00b2 & \u00e9 \" ' ( - \u00e8 _ \u00e7 \u00e0 ) = \u0008",
+        "\u0009 a z e r t y u i o p ^ $",
+        "q s d f g h j k l m  \u00f9 * {enter}",
+        "{shift} < w x c v b n , ; : ! {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ],
+      'shift': [
+        "{sp:1} 1 2 3 4 5 6 7 8 9 0 \u00b0 + \u0008",
+        "\u0009 A Z E R T Y U I O P \u00a8 \u00a3",
+        "Q S D F G H J K L M % \u00b5 {enter}",
+        "{shift} > W X C V B N ? . / \u00a7 {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ],
+      'alt': [
+        "\u00b2 & ~ # { [ | ` \\ ^ @ ] } \u0008",
+        "\u0009 a z \u20ac r t y u i o p ^ \u00a4",
+        "q s d f g h j k l m  \u00f9 * {enter}",
+        "{shift} < w x c v b n , ; : ! {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ],
+      'alt-shift': [
+        "{sp:1} 1 ~ # { [ | ` \\ ^ @ ] } \u0008",
+        "\u0009 A Z \u20ac R T Y U I O P \u00a8 \u00a4",
+        "Q S D F G H J K L M % \u00b5 {enter}",
+        "{shift} > W X C V B N ? . / \u00a7 {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ]
+    }
+  }
+
+  uLayout = userLayout[browserLang];
+  // console.log(uLayout);
+}
+
+
+function getTargetLayout(lang) {
+
+
+
+  var targetLayout = {
+    "ru": {
+      "name": "ms-Russian",
+      "lang": ["ru"],
+      "normal": [
+        "\u0451 1 2 3 4 5 6 7 8 9 0 - = \u0008",
+        "\u0009 \u0439 \u0446 \u0443 \u043a \u0435 \u043d \u0433 \u0448 \u0449 \u0437 \u0445 \u044a \\",
+        "\u0444 \u044b \u0432 \u0430 \u043f \u0440 \u043e \u043b \u0434 \u0436 \u044d \u000D",
+        "{shift} \\ \u044f \u0447 \u0441 \u043c \u0438 \u0442 \u044c \u0431 \u044e / {shift}",
+        "{accept} {space} {cancel}"
+      ],
+      "shift": [
+        "\u0401 ! \" \u2116 ; % : ? * ( ) _ + \u0008",
+        "\u0009 \u0419 \u0426 \u0423 \u041a \u0415 \u041d \u0413 \u0428 \u0429 \u0417 \u0425 \u042a /",
+        "\u0424 \u042b \u0412 \u0410 \u041f \u0420 \u041e \u041b \u0414 \u0416 \u042d \u000D",
+        "{shift} / \u042f \u0427 \u0421 \u041c \u0418 \u0422 \u042c \u0411 \u042e / {shift}",
+        "{accept} {space} {cancel}"
+      ]
+    },
+    "ruty": {
+      "name": "ms-Russian (Typewriter)",
+      "lang": ["ru"],
+      "normal": [
+        "| \u2116 - / \" : , . _ ? % ! ; \u0008",
+        "\u0009 \u0439 \u0446 \u0443 \u043a \u0435 \u043d \u0433 \u0448 \u0449 \u0437 \u0445 \u044a )",
+        "\u0444 \u044b \u0432 \u0430 \u043f \u0440 \u043e \u043b \u0434 \u0436 \u044d \u000D",
+        "{shift} ) \u044f \u0447 \u0441 \u043c \u0438 \u0442 \u044c \u0431 \u044e / {shift}",
+        "{accept} {space} {cancel}"
+      ],
+      "shift": [
+        "+ 1 2 3 4 5 6 7 8 9 0 = \\ \u0008",
+        "\u0009 \u0419 \u0426 \u0423 \u041a \u0415 \u041d \u0413 \u0428 \u0429 \u0417 \u0425 \u042a (",
+        "\u0424 \u042b \u0412 \u0410 \u041f \u0420 \u041e \u041b \u0414 \u0416 \u042d \u000D",
+        "{shift} ( \u042f \u0427 \u0421 \u041c \u0418 \u0422 \u042c \u0411 \u042e / {shift}",
+        "{accept} {space} {cancel}"
+      ]
+    },
+    "fr": {
+      'name': 'french-azerty-1',
+      'lang': ['fr'],
+      'normal': [
+        "\u00b2 & \u00e9 \" ' ( - \u00e8 _ \u00e7 \u00e0 ) = \u0008",
+        "\u0009 a z e r t y u i o p ^ $",
+        "q s d f g h j k l m  \u00f9 * {enter}",
+        "{shift} < w x c v b n , ; : ! {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ],
+      'shift': [
+        "{sp:1} 1 2 3 4 5 6 7 8 9 0 \u00b0 + \u0008",
+        "\u0009 A Z E R T Y U I O P \u00a8 \u00a3",
+        "Q S D F G H J K L M % \u00b5 {enter}",
+        "{shift} > W X C V B N ? . / \u00a7 {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ],
+      'alt': [
+        "\u00b2 & ~ # { [ | ` \\ ^ @ ] } \u0008",
+        "\u0009 a z \u20ac r t y u i o p ^ \u00a4",
+        "q s d f g h j k l m  \u00f9 * {enter}",
+        "{shift} < w x c v b n , ; : ! {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ],
+      'alt-shift': [
+        "{sp:1} 1 ~ # { [ | ` \\ ^ @ ] } \u0008",
+        "\u0009 A Z \u20ac R T Y U I O P \u00a8 \u00a4",
+        "Q S D F G H J K L M % \u00b5 {enter}",
+        "{shift} > W X C V B N ? . / \u00a7 {shift}",
+        "{accept} {alt} {space} {alt} {cancel}"
+      ]
+    }
+  };
+
+  tLayout = targetLayout[lang];
+  // console.log(tLayout);
+
+};
+
+
+
+
+
+//function to find index of a char from the template array
+function getIndexOfChar(arr, k) {
+  for (var i = 0; i < arr.length; i++) {
+    let index = arr[i].indexOf(k);
+    if (index > -1) {
+      return [i, index];
+    }
+  }
+}
+
+
+//step4: mapLayout function
+function mapLayout(inp, user, target) {
+  let out = "";
+  let indx = "";
+  
+
+ 
+//Cases when shift key is on
+  if (inp.shiftKey) {
+    indx = getIndexOfChar(user.shift, inp.key);
+    
+    if (!indx) {
+      // console.log("Not found in userTemplate");
+    }
+    else {
+      let row = indx[0];
+      let pos = indx[1];
+  
+      out = target.shift[row][pos];
+      // console.log(user.shift,inp.key,target.shift);
+    }
+
+  }
+  
+  
+  //cases when caps lock is on
+  else if (inp.capsKey){
+    indx = getIndexOfChar(user.shift, inp.key);
+
+        if (!indx) {
+          // console.log("Not found in userTemplate");
+        }
+        else {
+          let row = indx[0];
+          let pos = indx[1];
+        
+          out = target.shift[row][pos];
+          // console.log(user.shift,inp.key,target.shift);
+        }
+  }
+
+
+
+  //if all cases fail default to normal
+  else {
+    indx = getIndexOfChar(user.normal, inp.key); //works
+   
+    // console.log(indx);//enter key returns here
+
+    if (!indx) {
+      //Case returns where user input not found in userTemplate / user template mismatch
+      // console.log("Not found in userTemplate");
+    }
+    else {
+      // console.log(indx);
+      row = indx[0];
+      pos = indx[1];
+    
+      //send this for V keyboard highlight
+      out = target.normal[row][pos];
+      // console.log(user.normal,inp.key,target.normal);
+    }
+
+  }
+
+
+
+
+
+
+
+ 
+  return out;
+
+}
