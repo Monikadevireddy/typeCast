4b5e167d4f2a962b778b184473d6d54214d49579
Dinesh <dinesh.condur@gmail.com>
2017-11-06 18:16:28 -0500

    changed register parameter

--- docs/js/app_init.js
+++ docs/js/app_init.js
@@ -12,7 +12,7 @@ $(document).ready(function () {
   
   if ('serviceWorker' in navigator) {
     navigator.serviceWorker
-      .register('js/sw.js',{scope :'./'})
+      .register('js/sw.js')
       .then(function(registration) {console.log(registration);})
       .catch(function(err){console.log(err);})
   } 
